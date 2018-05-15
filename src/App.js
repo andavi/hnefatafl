006aaa1b2693424429d59a00701aadc22b5b8459
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-05-15 10:00:40 -0400

    paint squares

--- src/App.js
+++ src/App.js
@@ -5,19 +5,50 @@ const NUM_SQUARES = 11;  // NUM_SQUARES x NUM_SQUARES
 const SQUARE_SIZE = 50;
 
 class App extends Component {
+  paint = (row, col) => {
+    if ((row === 0 && col === 0)
+      || (row === 0 && col === 10)
+      || (row === 10 && col === 0)
+      || (row === 10 && col === 10)
+      || (row === 5 && col === 5)
+    ) {
+      return 'bg-red';
+    } else if ((row === 0 && (col > 2 && col < 8))
+      || (row === 10 && (col > 2 && col < 8))
+      || (col === 0 && (row > 2 && row < 8))
+      || (col === 10 && (row > 2 && row < 8))
+      || (row === 1 && col === 5)
+      || (row === 9 && col === 5)
+      || (row === 5 && col === 1)
+      || (row === 5 && col === 9)
+      ) {
+      return 'bg-near-black';
+    } else if ((row === 3 && col === 5)
+      || (row === 4 && (col > 3 && col < 7))
+      || (row === 5 && (col > 2 && col < 5))
+      || (row === 5 && (col > 5 && col < 8))
+      || (row === 6 && (col > 3 && col < 7)) 
+      || (row === 7 && col === 5) 
+      ) {
+      return 'bg-blue';
+    }
+    return '';
+  }
+
   render() {
     return (
       <div>
         <div className='tc'>
           <p className='f2'>Hnefatafl</p>
+          <p className='f4'>Viking Chess</p>
         </div>
-        <Board numSquares={NUM_SQUARES} squareSize={SQUARE_SIZE}/>
+        <Board paint={this.paint} numSquares={NUM_SQUARES} squareSize={SQUARE_SIZE}/>
       </div>
     );
   }
 }
 
-const Board = ({ numSquares, squareSize }) => {
+const Board = ({ numSquares, squareSize, paint }) => {
   const rows = [];
 
   for (let i=0; i < numSquares; i++) {
@@ -26,6 +57,8 @@ const Board = ({ numSquares, squareSize }) => {
         numSquares={numSquares} 
         squareSize={squareSize} 
         key={i}
+        row={i}
+        paint={paint}
       />)
   }
   return (
@@ -35,14 +68,16 @@ const Board = ({ numSquares, squareSize }) => {
     >{rows}</div>);
 }
 
-const Row = ({ numSquares, squareSize }) => {
+const Row = ({ numSquares, squareSize, row, paint }) => {
   const squares = [];
   const edgeSize = 1 / numSquares * 100 + '%';
 
   for (let i = 0; i < numSquares; i++) { 
+    const color = paint(row, i);
+
     squares.push(
       <div 
-        className='fl ba' 
+        className={'fl ba ' + color}
         style={{ width: edgeSize, height: squareSize }}
         key={i}
       ></div>);
