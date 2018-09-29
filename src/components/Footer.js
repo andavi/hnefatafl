58bf14e434ac727e74cb7dec299eb42e5faf6e33
Andrew Davis <andrew.bradley.davis@gmail.com>
2018-09-28 18:24:12 -0700

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,7 @@ const Footer = () =>
 	<footer className="center pv4 ph3 ph5-m ph6-l gray avenir">
 	  <small className="f6 db tc">
 	  	© 2018 
-		  	<a className="f6 dib ph2 link dark-gray dim" href="https://andavi.github.io/">
-		  		Andrew Davis
-		  	</a>
+
 	  	</small>
 	</footer>
 
